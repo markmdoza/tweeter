989ee0e6c03a67bb190627a0d25ccdf8e53170b7
Mark Mendoza <marklmdoza@gmail.com>
2023-08-11 22:10:04 -0400

    Implement character counter with jQuery

--- public/scripts/compser-char-counter.js
+++ public/scripts/compser-char-counter.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
-  console.log('Hello world');
+  $('.new-tweet textarea').on('input', function() {
+    const maxChars = 140;
+  // This code snippet calculates the length of the value of the current element using jQuery and stores it in the variable "currentChars".
+    const currentChars = $(this).val().length;
+    const remainingChars = maxChars - currentChars;
+    $('.counter').text(remainingChars);
+
+    if (remainingChars < 0) {
+      $('.counter').addClass('invalid');
+    } else {
+      $('.counter').removeClass('invalid');
+    }
+  })
+  console.log('Document is ready');
 });
