b6169d913cb4468064c484a1675c63ddc4546f56
Mark Mendoza <marklmdoza@gmail.com>
2023-08-15 21:10:06 -0400

    Styled new CSS section for new tweets to show and added hover effects

--- public/scripts/compser-char-counter.js
+++ public/scripts/compser-char-counter.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $('.new-tweet textarea').on('input', function() {
     const maxChars = 140;
-  // This code snippet calculates the length of the value of the current element using jQuery and stores it in the variable "currentChars".
+    // This code snippet calculates the length of the value of the current element using jQuery and stores it in the variable "currentChars".
     const currentChars = $(this).val().length;
     const remainingChars = maxChars - currentChars;
     $('.counter').text(remainingChars);
